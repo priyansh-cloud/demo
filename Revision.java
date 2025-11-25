e845b812981b81b35443741305883d8d33d6f06e
priyanshu-cloud <priyanshu.mahto7781@gmail.com>
2025-11-25 17:09:39 +0530

    Factorial_recursion

--- Revision.java
+++ Revision.java
@@ -1,8 +1,24 @@
 public class Revision {
 
+ static int fib(int n){
+   
+    // base case
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    // recursive call
+    else return fib(n-1)+fib(n-2);
+  
+}
+
     public static void main(String[] args) {
-        System.out.println("Priyanshu");
-        System.out.println("Kumar");
+        
+        fib(3);
+        System.out.println(fib(4));
+        
     }
     
 }
